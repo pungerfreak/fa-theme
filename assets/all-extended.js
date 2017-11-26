a5719b1f8561c757b053d770fd070ee1cca0d7cc
Daniel Greenlaw <daniel@shirtspace.com>
2017-11-26 14:34:08 -0800

    Begin adjusting layout and functionality on product page

---
+++ assets/all-extended.js
@@ -0,0 +1,27 @@
+jQuery(function($) {
+  // var left = $('.slick--left-svg');
+  // var next = $('.slick--next-svg');
+  // $('.product-images-slider').slick('unslick');
+  // $('.product-images-slider').slick({
+  //   dots: true
+  // });
+
+  // left.appendTo('.slick-prev');
+  // next.appendTo('.slick-next');
+
+  $('.slider-for').slick('unslick');
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
